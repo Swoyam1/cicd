9054d6c7a9e28a90c410ff7bd65fcd3974a30bf4
swoyam1 <swoyam1.pant@gmail.com>
2023-03-29 22:52:46 +0530

    init

---
+++ github-cicd/src/main/java/com/cicd/githubcicd/GitCicdApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.githubcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCicdApplication.class, args);
+	}
+
+}
